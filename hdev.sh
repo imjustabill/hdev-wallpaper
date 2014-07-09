a3e216e995cacd0c34ebf91c437462183aed0817
Bill Eberly <imjustabill@gmail.com>
2014-07-09 01:41:33 -0400

    Removed old version

--- hdev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cvlc -V dummy --video-filter scene  --no-audio --scene-path . --scene-prefix hdev --scene-format jpg --scene-replace --run-time 1 --deinterlace 1 --deinterlace-mode yadif playlist.m3u8 &  sleep 10 && killall -9 vlc &&  mv hdev.jpg /var/www/hdev/hdev.jpg 
-#--scene-path /home/imjustabill 
